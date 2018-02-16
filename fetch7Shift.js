8e3a93bc13acbc085228d1d3841abfd9cb7be32e
Mike Etlinger <mk-etlinger@gmail.com>
2018-02-16 10:57:49 -0500

    clean up var declarations, add baseURL, abstract out parseJSON, buildShifts and insertHours

--- fetch7Shift.js
+++ fetch7Shift.js
@@ -1,78 +1,34 @@
 function fetch7Shifts() {
   // Basic auth headers for API
-  var apikey = ''
-  var headers = {
-    "Authorization" : "Basic " + Utilities.base64Encode(apikey + ':')
-  };
+  var apiKey = '',
+      headers = {
+        "Authorization" : "Basic " + Utilities.base64Encode(apiKey + ':')
+      };
 
   var params = {
     "method":"GET",
     "headers": headers
   };
   
-  // Limit must be < 500, problems with > 150 // YYYY-MM-DD // deep=1 returns associated objs like User info
-  var limit = '&limit=5', date = 'start[gte]=2017-10-01', includeAssocObjs = '&deep=1'
-  var excludeOpenShifts = '&open=0', excludeDeletedShifts = '&deleted=0'
-  var queries = date + limit + includeAssocObjs + excludeOpenShifts + excludeDeletedShifts;
+  var baseURL = 'https://api.7shifts.com/v1/shifts/?'
   
-  var url = 'https://api.7shifts.com/v1/shifts/?' + queries;
+  // Limit must be < 500, problems with > 150 // > YYYY-MM-DD // deep=1 returns associated objs like User info
+  var limit = '&limit=500', greaterThanDate = 'start[gte]=2018-02-01', includeAssocObjs = '&deep=1'
+  var excludeOpenShifts = '&open=0', excludeDeletedShifts = '&deleted=0'
+  var queries = greaterThanDate + limit + includeAssocObjs + excludeOpenShifts + excludeDeletedShifts;
+  
+  var url = baseURL + queries;
   
   // Make request to API and get response
-  var response = UrlFetchApp.fetch(url, params);
+  var response = UrlFetchApp.fetch( url, params );
   
- // Parse JSON string to JSON Obj
+  return response
+}
+
+function parseJSON( response ) {
+// Parse JSON string to JSON Obj
   var jsonString = response.getContentText();
   var jsonObj = JSON.parse(jsonString);
-  
+  //console.log('json obj: ', jsonObj)
   return jsonObj
 }
-
-function buildShifts(jsonObj) {
- // Map individual shifts and their assoc. objects to an array
-  var shiftObjsArray = jsonObj.data.map(function(shiftData) {
-     return new Shift(shiftData.user.id, 
-                      shiftData.user.firstname,
-                      shiftData.user.lastname,
-                      shiftData.shift.start, 
-                      shiftData.shift.end, 
-                      shiftData.shift.id, 
-                      shiftData.shift.role_id)
-  })
-  return shiftObjsArray
-}
-
-function insertHoursWorked() {  
-  var shiftsArray = buildShifts(fetch7Shifts())
-  shiftsArray.forEach(function(shift) {
-    Logger.log("Member: " + shift.memberFirstName + " " + shift.memberLastName + " worked " + shift.hoursWorked()  + " hours")
-  })
-  
-  // TODO
-  // * Insert hours worked to proper cells
-}
-
-function Shift(userId, userFirstName, userLastName, start, end, shiftId, roleId) {  
-  this.userId = userId;
-  this.memberFirstName = userFirstName;
-  this.memberLastName = userLastName
-  this.start = start;
-  this.end = end;
-  this.shiftId = shiftId
-  this.roleId = roleId
-  this.hoursWorked = function() {
-    var secondsWorked = this.convertTimeToSecs(this.end) - this.convertTimeToSecs(this.start);
-    var numOfSeconds = Number(secondsWorked);
-
-    return Math.floor(numOfSeconds / 3600)
-  }
-  this.convertTimeToSecs = function(date) {
-    var time = date.split(' ')[1]
-    var [hours, minutes, seconds] = time.split(/:/);
-
-    var durationInSec = hours * 60 * 60 + minutes * 60 + (+seconds);
-    return durationInSec
-  }
-}
-
-// Call function to insert into proper cells
-insertHoursWorked()
