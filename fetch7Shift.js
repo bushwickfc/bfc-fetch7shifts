f81a5f9b4e89b1c83603f87a25a580ebbe517480
Mike Etlinger <mk-etlinger@gmail.com>
2018-03-18 17:07:52 -0400

    abstract out URL assembly to buildURL(), add prompt from user to get month, add less than date to only grab 1 month of shift data

--- fetch7Shift.js
+++ fetch7Shift.js
@@ -2,33 +2,49 @@ function fetch7Shifts() {
   // Basic auth headers for API
   var apiKey = '',
       headers = {
-        "Authorization" : "Basic " + Utilities.base64Encode(apiKey + ':')
+        "Authorization" : "Basic " + Utilities.base64Encode( apiKey + ':' )
       };
 
   var params = {
-    "method":"GET",
+    "method": "GET",
     "headers": headers
   };
   
-  var baseURL = 'https://api.7shifts.com/v1/shifts/?'
-  
-  // Limit must be < 500, problems with > 150 // > YYYY-MM-DD // deep=1 returns associated objs like User info
-  var limit = '&limit=500', greaterThanDate = 'start[gte]=2018-02-01', includeAssocObjs = '&deep=1'
-  var excludeOpenShifts = '&open=0', excludeDeletedShifts = '&deleted=0'
-  var queries = greaterThanDate + limit + includeAssocObjs + excludeOpenShifts + excludeDeletedShifts;
-  
-  var url = baseURL + queries;
-  
   // Make request to API and get response
-  var response = UrlFetchApp.fetch( url, params );
+  var response = UrlFetchApp.fetch( buildURL(), params );
   
   return response
+}
+
+function buildURL() {
+  var baseURL = 'https://api.7shifts.com/v1/shifts/?',
+    month = getMonthFromUser()
+  
+  // Limit must be < 500, // date format: YYYY-MM-DD // deep=1 returns assoc. objs like User info
+  var startDate = 'start[gte]=2018-' + month + '-01',
+    endDate = '&start[lte]=2018-' + ( month + 1 ) + '-01',
+    limit = '&limit=500', 
+    includeAssocObjs = '&deep=1',
+    excludeOpenShifts = '&open=0', 
+    excludeDeletedShifts = '&deleted=0'
+
+  var queries = startDate + endDate + limit + includeAssocObjs + excludeOpenShifts + excludeDeletedShifts,
+    url = baseURL + queries;
+  
+  return url
 }
 
 function parseJSON( response ) {
 // Parse JSON string to JSON Obj
   var jsonString = response.getContentText();
-  var jsonObj = JSON.parse(jsonString);
-  //console.log('json obj: ', jsonObj)
-  return jsonObj
+  var jsonObj = JSON.parse( jsonString );
+
+  return jsonObj;
+}
+
+function getMonthFromUser() {
+  var ui = SpreadsheetApp.getUi();
+  var response = ui.prompt( 'Please enter the month as a number, e.g: 01, 02, 03, 10,' );
+
+  return response.getResponseText();
 }
